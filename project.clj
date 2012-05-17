8fbf72ee1e93901d12ac0cc7aefd2c3964cad392
James Greenhill <fuziontech@gmail.com>
2012-05-17 15:42:10 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clearman "0.0.1"
   :description "Gearman Client/Worker Bindings for Clojure"
   :dependencies [[clojure "1.3.0"]
-                 [aleph "0.2.1-SNAPSHOT"]
+                 [aleph "0.3.0-alpha2"]
+                 [lamina "0.5.0-alpha3"]
+                 [gloss "0.2.2-alpha2"] 
                  [all-the-while "0.0.1"]]
   :dev-dependencies [[lein-clojars "0.7.0"]])
